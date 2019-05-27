78366420f1a6e6d4907eac021c958d403efac5f3
Michelle Moon <michellemoon236@gmail.com>
2019-05-27 17:52:26 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,8 +5,8 @@ class Application
   def call(env)
     resp = Rack::Response.new
  
-    binding.pry
-    if Time.now <= 12
+    time = Time.now
+    if time.hour <= 12
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
